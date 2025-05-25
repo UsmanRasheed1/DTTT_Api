b60a774e6d617b4c2c25e39c23954de50de76565
UsmanRasheed1 <usmanrasheed2002@gmail.com>
2025-05-25 15:39:31 +0500

    Updated ports

--- app.py
+++ app.py
@@ -53,5 +53,5 @@ def analyze_sentiment():
     return jsonify({"sentiment": sentiment})
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 4000))
-    app.run(host="0.0.0.0", port=port)
+    
+    app.run(host="0.0.0.0", debug=True)
